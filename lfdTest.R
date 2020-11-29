298a480b2e1670c61d8928c77c6cbb3e940bba24
julianflowers <julian.flowers12@gmail.com>
2020-11-29 20:49:39 +0000

    lfd

---
+++ lfdTest.R
@@ -0,0 +1,23 @@
+library(ukcovid19)
+
+filters <- "areaType=utla"
+structure <- list(
+
+       date = "date",
+       code = "areaCode",
+       name = "areaName",
+       newLFD = "newLFDTests", 
+       cumLFD = "cumLFDTests"
+
+)
+
+lfdTests <- get_data(
+
+    filters = filters, 
+    structure = structure
+)
+
+lfdTests  %>% 
+      mutate(date = lubridate::ymd(date)) %>%
+      ggplot(aes(date, newLFD, fill = name)) +
+      geom_col(show.legend = FALSE)
