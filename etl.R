f891fd991ae34b7f1be524aa45984ec8fdccb7d9
julianflowers <julian.flowers12@gmail.com>
2020-12-03 18:35:17 +0000

    etl-update

--- etl.R
+++ etl.R
@@ -14,6 +14,7 @@ latest_data <- get_page_links(url) %>%
   enframe() %>%
   mutate(latest = paste0("https://www.gov.uk", value), 
          links = map(latest, get_page_links)) %>%
+  View()
   unnest("links") %>%
   filter(str_detect(links, "ods$"))
 
@@ -25,4 +26,68 @@ filenames <- basename(latest_data$links)
 for(i in 1:8){
 download(latest_data$links[i], paste0("data/", filenames[i]))
 }
+
+## extract files
+
+files <- list.files("data", pattern = "ods")
+files
+
+ltlap2 <- read_ods(paste0("data/",files[6]), sheet = 2) %>%
+  mutate_at(.vars = 7:ncol(.), as.numeric) %>%
+  pivot_longer(names_to = "metric", values_to = "value", 7:ncol(.)-1) %>%
+  mutate(date = lubridate::dmy(metric)) %>%
+  janitor::clean_names()
+
+ltlap2 %>%
+  tail()
+
+ltlap2 %>%
+  filter(!is.na(date), utla_name != "ENGLAND") %>%
+  ggplot(aes(date, value, colour = utla_name)) +
+  geom_line(show.legend = FALSE)
+
+sheets1 <- list_ods_sheets(paste0("data/",files[2]))   
+
+sheets2 <- list_ods_sheets(paste0("data/",files[4]))   
+
+
+## LA tests
+la_cases <- read_ods((paste0("data/",files[2])), sheet = sheets1[9], skip = 2)
+la_tests <- read_ods((paste0("data/",files[2])), sheet = sheets1[8], skip = 2)
+
+la_cases_l <- la_cases %>%
+  mutate_at(.vars = 7:ncol(.), as.numeric) %>%
+  pivot_longer(names_to = "metric", values_to = "value", 7:ncol(.))
+
+la_tests_l <- la_tests %>%
+  mutate_at(.vars = 7:ncol(.), as.numeric) %>%
+  pivot_longer(names_to = "metric", values_to = "value", 7:ncol(.))
+
+pos <- data.frame(la_cases_l, tests = la_tests_l$value) %>%
+  mutate(pos = round(100 * value / tests, 2)) %>%
+  separate(metric, c("start", "end"), sep = "-") %>%
+  mutate(start = ifelse(str_detect(start, "2020"), start, paste0(str_trim(start), "20")), 
+         end = ifelse(str_detect(end, "2020"), end, paste0(str_trim(end), "20")), 
+         start = lubridate::dmy(start), 
+         end = lubridate::dmy(end)) %>%
+  filter(!is.na(start))
+
+p <- pos %>%
+  ggplot(aes(end, pos, colour = LTLA.Name)) +
+  geom_line()
+  
+plotly::ggplotly(p)
+
+## tests processed
+
+processed <- read_ods((paste0("data/",files[8])), sheet = sheets1[3])
+processed
+
+read_ods((paste0("data/",files[4])), sheet = sheets2[8], skip = 1) 
+%>%
+  janitor::remove_empty("rows") %>%
+  select(-contains("NA")) %>%
+  .[2:10]
+
+
 
