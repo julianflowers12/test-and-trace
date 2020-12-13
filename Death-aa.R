2608fa7c631107fc853d874c41bd1e5d4e70cf92
julianflowers12 <julian.flowers13@gmail.com>
2020-12-13 10:40:25 +0000

    Mortality

---
+++ Death-aa.R
@@ -0,0 +1,36 @@
+library(tidyverse)
+library(GGally)
+
+## download cumulative death rates for each local authority
+
+data <- read_csv("https://api.coronavirus.data.gov.uk/v2/data?areaType=utla&metric=cumDeaths60DaysByDeathDateRate&metric=cumOnsDeathsByRegistrationDateRate&metric=cumDeaths28DaysByDeathDateRate&format=csv")
+
+summary(data)
+
+## convert dates to week numbers
+
+latest_data <- data %>%
+  mutate(week = ifelse(!is.na(cumOnsDeathsByRegistrationDateRate), 1, 0)) %>%
+  mutate(weekno = lubridate::week(date))
+
+## transform to long format, filter for England LAs, calculate mean weekly rate
+
+ldw <- latest_data %>%
+  pivot_longer(names_to = "metric", values_to = "data", cols = 5:7) %>%
+  filter(str_detect(areaCode, "^E")) %>%
+  group_by(areaName, metric, weekno) %>%
+  summarise(weeksum = mean(data, na.rm = TRUE ))
+
+## plot latest week as scatter plot
+
+ldw %>%
+  filter(weekno == 48) %>%
+  pivot_wider(names_from = "metric", values_from = "weeksum") %>%
+  ggscatmat(columns = 3:5) +
+  geom_smooth(method = "lm", se = FALSE)
+
+## ONS death rates (COVID on death certificate) are
+## highly correlated with death rates in people with 
+## +ve covid tests within 28 and 60 days
+
+
