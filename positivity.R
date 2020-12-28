a411ab00008a03952ac395b4251514cc9303390f
julianflowers12 <73796630+julianflowers12@users.noreply.github.com>
2020-12-28 20:03:08 +0000

    Update positivity.R

--- positivity.R
+++ positivity.R
@@ -4,8 +4,6 @@ library(lubridate)
 
 data1 <- read_csv("https://daisy.coronavirus.data.gov.uk/api/v1/data?filters=areaType=nation;areaName=England&structure=%7B%22areaType%22:%22areaType%22,%22areaName%22:%22areaName%22,%22areaCode%22:%22areaCode%22,%22date%22:%22date%22,%22newCasesBySpecimenDate%22:%22newCasesBySpecimenDate%22,%22cumCasesBySpecimenDate%22:%22cumCasesBySpecimenDate%22,%22newPeopleTestedBySpecimenDate%22:%22newPeopleTestedBySpecimenDate%22,%22cumPeopleTestedBySpecimenDate%22:%22cumPeopleTestedBySpecimenDate%22%7D&format=csv")
 
-head(data)
-
 pos <- data1 %>%
   mutate(date = lubridate::ymd(date)) %>%
   arrange(date) %>%
