285a72ff08f98729aef09397f19417576feb127d
julianflowers12 <73796630+julianflowers12@users.noreply.github.com>
2020-12-28 19:59:33 +0000

    Update positivity.R

--- positivity.R
+++ positivity.R
@@ -2,11 +2,11 @@
 library(tidyverse)
 library(lubridate)
 
-data <- read_csv("https://daisy.coronavirus.data.gov.uk/v1/data?filters=areaType=nation;areaName=England&structure=%7B%22areaType%22:%22areaType%22,%22areaName%22:%22areaName%22,%22areaCode%22:%22areaCode%22,%22date%22:%22date%22,%22newCasesBySpecimenDate%22:%22newCasesBySpecimenDate%22,%22cumCasesBySpecimenDate%22:%22cumCasesBySpecimenDate%22,%22newPeopleTestedBySpecimenDate%22:%22newPeopleTestedBySpecimenDate%22,%22cumPeopleTestedBySpecimenDate%22:%22cumPeopleTestedBySpecimenDate%22%7D&format=csv")
+data1 <- read_csv("https://daisy.coronavirus.data.gov.uk/v1/data?filters=areaType=nation;areaName=England&structure=%7B%22areaType%22:%22areaType%22,%22areaName%22:%22areaName%22,%22areaCode%22:%22areaCode%22,%22date%22:%22date%22,%22newCasesBySpecimenDate%22:%22newCasesBySpecimenDate%22,%22cumCasesBySpecimenDate%22:%22cumCasesBySpecimenDate%22,%22newPeopleTestedBySpecimenDate%22:%22newPeopleTestedBySpecimenDate%22,%22cumPeopleTestedBySpecimenDate%22:%22cumPeopleTestedBySpecimenDate%22%7D&format=csv")
 
 head(data)
 
-pos <- data %>%
+pos <- data1 %>%
   mutate(date = lubridate::ymd(date)) %>%
   arrange(date) %>%
   mutate(pos =  newCasesBySpecimenDate / newPeopleTestedBySpecimenDate, 
@@ -15,8 +15,3 @@ pos <- data %>%
   filter(date < today() - days(5))
 
 
-pos %>% 
-  ggplot() +
-  #geom_col(aes(date, pos)) +
-  geom_line(aes(date, cumpos)) +
-  geom_line(aes(date, sevenDayPos, colour = "red"))
